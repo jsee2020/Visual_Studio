2ee3a128b515a856df7cf2c9af636cf2fe376146
jsee2020 <61015269+jsee2020@users.noreply.github.com>
2021-10-15 22:45:25 +0900

    Add files via upload

---
+++ Python/PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -0,0 +1,20 @@
+
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5 import uic
+form_class = uic.loadUiType("Test.ui")[0]
+
+class MyWindow(QDialog, form_class):
+   def __init__(self):
+       super().__init__()
+       self.setupUi(self)
+       self.pushButton.clicked.connect(self.btn_clicked)
+
+   def btn_clicked(self):
+       self.textEdit.setText("Hello World!")
+
+if __name__ == "__main__":
+   app = QApplication(sys.argv)
+   myWindow = MyWindow()
+   myWindow.show()
+   app.exec_()
