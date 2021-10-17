6a9ea9ef59864d5c1bb9ccff9c29459c18604a02
jseekek <jseekek@daum.net>
2021-10-17 16:58:17 +0900

    UI SQL TEST

--- Python/PythonApplication1/PythonApplication1/PythonApplication1.py
+++ Python/PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -1,4 +1,4 @@
-
+import pymysql 
 import sys
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
@@ -9,9 +9,23 @@ class MyWindow(QDialog, form_class):
        super().__init__()
        self.setupUi(self)
        self.pushButton.clicked.connect(self.btn_clicked)
+       self.pushButton_2.clicked.connect(self.btn2_clicked)
 
    def btn_clicked(self):
        self.textEdit.setText("Hello World!")
+
+   def btn2_clicked(self):
+       conn = pymysql.connect(host='jsee.ipdisk.co.kr', user='root', password='whdtnsql!', db='test', charset='utf8') 
+       cursor = conn.cursor() 
+       sql = "SELECT * FROM qry_test" 
+       cursor.execute(sql) 
+       res = cursor.fetchall() 
+       conn.commit() 
+       conn.close()
+       setTables(res)
+
+
+       #self.textEdit_2.setText(data)
 
 if __name__ == "__main__":
    app = QApplication(sys.argv)
